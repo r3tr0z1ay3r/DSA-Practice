a157fb4e529d13ec141fbf5d927db5eebc6df552
r3tr0z1ay3r <mukeshtouch2020@gmail.com>
2024-09-11 21:01:57 +0530

    Finished 2d matrix code

--- BinarySearch/search2Dmatrix.py
+++ BinarySearch/search2Dmatrix.py
@@ -1,16 +1,20 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
         top,bot = 0,len(matrix)-1
+        #mid_row = top + (bot-top) // 2
         while top <= bot:
             mid_row = top + (bot-top) // 2
-            if matrix[mid_row][-1] > target:
-                bot -= 1
-            if matrix[mid_row][-1] < target:
-                top += 1
-            else:
+            if target in matrix[mid_row]:
                 break
-        if not (top <= bot):        
-            return False
+            if matrix[mid_row][-1] > target:
+                bot = mid_row - 1
+                print("UPdating bot to {}".format(bot))
+                continue
+            if matrix[mid_row][-1] < target:
+                top = mid_row + 1
+                print("Updatin Top to {}".format(top))
+                continue
+
         l,r = 0 , len(matrix[0])-1
         while l <= r:
             mid = l + (r-l) // 2
@@ -21,3 +25,10 @@ class Solution:
             else:
                 return True
         return False
+
+obj = Solution()
+matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
+matrix1 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target1 = 3
+target = 10
+print(obj.searchMatrix(matrix1,target1))
